c7ad717465d9c4c54587f0566a55d71504099e73
hex <47760583+Hexsl@users.noreply.github.com>
2022-11-29 21:41:37 -0500

    Update qline.c

--- src/modules/third/qline.c
+++ src/modules/third/qline.c
@@ -9,7 +9,7 @@
 * -----------------------------------------------------------------------------------------------------------------------------------------------
 * MIT License
 * 
-* Copyright (c) 2022 Paul 'Hexick'  Q.
+* Copyright (c) 2022 Hexick [pseudonym]
 * 
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
